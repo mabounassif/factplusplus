49a8e42806c82b73a29ecbc06f232c3a9ec6c1fc
Dmitry Tsarkov <dmitry.tsarkov@gmail.com>
2016-12-09 11:45:59 +0000

    Update C interface test to cover #71

--- FaCT++.C/test.c
+++ FaCT++.C/test.c
@@ -40,35 +40,51 @@ void print2Darray ( const char*** names )
 
 int main ( void )
 {
+	puts("Starting FaCT++ C interface tests");
 	// create kernel
 	fact_reasoning_kernel* k = fact_reasoning_kernel_new();
 
 	// create classes C,D, property R
+	puts("Creating entities");
 	fact_concept_expression* c = fact_concept(k,"C");
 	fact_concept_expression* d = fact_concept(k,"D");
+	fact_individual_expression* i = fact_individual(k,"I");
 	fact_o_role_expression* r = fact_object_role(k,"R");
 
 	// create C [= ER.T, ER.T [= D
+	puts("Creating axioms");
 	fact_concept_expression* some = fact_o_exists ( k, r, fact_top(k));
 	fact_implies_concepts ( k, c, some );
 	fact_implies_concepts ( k, some, d );
+	fact_instance_of ( k, i, c );
 
 	// classify KB is not necessary: it's done automatically depending on a query
+	puts("Classifying ontology");
 	fact_classify_kb(k);
 
 	// check whether C [= D
+	puts("Is C subsumed by D?");
 	if ( fact_is_subsumed_by(k,c,d) )
-		puts("yes!\n");
+		puts("Yes!\n");
 	else
 		puts("No...\n");
 
-	// create a concept actor and use it to get all superclasses of D
+	// create a concept actor and use it to get all superclasses of C
+	puts("All superclasses of C:");
 	fact_actor* actor = fact_concept_actor_new();
 	fact_get_sup_concepts(k,c,0,&actor);
 	print2Darray(fact_get_elements_2d(actor));
 	fact_actor_free(actor);
 
+	// create an individual actor and use it to get all instances of C
+	puts("All instances of C:");
+	fact_actor* i_actor = fact_individual_actor_new();
+	fact_get_instances(k, c, &i_actor);
+	print2Darray(fact_get_elements_2d(i_actor));
+	fact_actor_free(i_actor);
+
 	// get all the properties
+	puts("All object properties:");
 	fact_o_role_expression* o_top = fact_object_role_top(k);
 	actor = fact_o_role_actor_new();
 	fact_get_sub_roles(k,(fact_role_expression*)o_top,0,&actor);
@@ -76,6 +92,8 @@ int main ( void )
 	fact_actor_free(actor);
 
 	// we done so let's free memory
+	puts("Destroying reasoning kernel");
 	fact_reasoning_kernel_free(k);
+	puts("All done");
 	return 0;
 }
